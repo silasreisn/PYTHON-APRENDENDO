8612e2ccef94de173d225e157826ef24615aeee5
Perkles <147007042+Syllas3@users.noreply.github.com>
2026-01-06 19:57:30 -0300

    Novos atividades concluidas

--- ex019.py
+++ ex019.py
@@ -1,1 +1,21 @@
 #Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um progrma que ajude ele, lendo o nome deles e escrevendo  o nome do escolhido.
+
+'''
+import random
+a1 = str (input('Digite o nome do primeiro aluno: '))
+a2 = str (input('Digite o nome do segundo aluno: '))
+a3 = str (input('Digite o nome do terceiro aluno: '))
+a4 = str (input('Digite o nome do quarto aluno: '))
+lista = [a1, a2, a3, a4]
+escolhido = random.choice(lista)
+print('O aluno escolhido foi {}'.format(escolhido))
+'''
+
+from random import choice
+a1 = str (input('Digite o nome do primeiro aluno: '))
+a2 = str (input('Digite  o nome do segunto aluno: '))
+a3 = str (input('Digite o nome do terceiro aluno: '))
+a4 = str (input('Digite o nome do quarto aluno: '))
+lista = [a1, a2, a3, a4]
+escolhido = choice(lista)
+print('O aluno escolhido foi {}'.format(escolhido))
--- ex021.py
+++ ex021.py
@@ -1,1 +1,7 @@
 #Faça um programa em PYTHON que abrar e reproduza o áudio de um arquivo MP3.
+
+import pygame
+pygame.init()
+pygame.mixer.music.load('ex021.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
