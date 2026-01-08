458271de1d9295eef4dfb6878a3c9c6958a748e3
Perkles <147007042+Syllas3@users.noreply.github.com>
2026-01-08 20:15:34 -0300

    Adicionar aula09 de Python

---
+++ aula python/aula09a.py
@@ -0,0 +1,67 @@
+'''frase = 'Curso em Vídeo Python'
+print(frase)'''
+
+'''frase = 'Curso em Vídeo Python'
+print(frase[3])'''
+
+'''frase = 'Curso em Vídeo Python'
+print(frase[3:13])'''
+
+'''frase = 'Curso em Vídeo Python'
+print(frase[:13])'''
+
+'''frase = 'Curso em Vídeo Python'
+print(frase[13:])'''
+
+'''frase = 'Curso em Vídeo Python'
+print(frase[1:15])'''
+
+'''frase = 'Curso em Vídeo Python'
+print(frase[1:15:2])'''
+
+'''frase = 'Curso em Vídeo Python'
+print(frase[1::2])'''
+
+'''frase = 'Curso em Vídeo Python'
+print(frase[::2])'''
+
+'''print("""Loren ipsun dolor sit anet, consectetur adipisci elit, 
+      sed eiusnod tenpor incidunt ut labore et dolore nagna aliqua.
+      Ut enin ad ninin venian, quis nostrun exercitationen ullan 
+      corporis suscipit laboriosan, nisi ut aliquid ex ea connodi consequatur.""")'''
+
+'''frase = 'Curso em Vídeo Python'
+print(frase.count('O'))'''
+
+'''frase = 'Curso em Vídeo Python'
+print(frase.upper().count('O'))'''
+
+'''frase = 'Curso em Vídeo Python'
+print(len(frase))'''
+
+'''frase = 'Curso em Vídeo Python'
+print(frase.replace('Python', 'Android'))'''
+
+'''frase = 'Curso em Vídeo Python'
+frase = frase.replace('Python', 'Android')
+print(frase)'''
+
+'''frase = 'Curso em Vídeo Python'
+print('Curso' in frase)'''
+
+'''frase = 'Curso em Vídeo Python'
+print(frase.find('Curso'))'''
+
+'''frase = 'Curso em Vídeo Python'
+print(frase.lower().find('vídeo'))'''
+
+'''frase = 'Curso em Vídeo Python'
+print(frase.split())'''
+
+'''frase = 'Curso em Vídeo Python'
+dividido = frase.split()
+print(dividido [0])'''
+
+'''frase = 'Curso em Vídeo Python'
+dividido = frase.split()
+print(dividido [2][3])'''
