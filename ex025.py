fad47daaebab6fc0e1d321c2551531cabb3fa4f1
Perkles <147007042+Syllas3@users.noreply.github.com>
2026-01-11 19:18:29 -0300

    Resolução de novos exercícios

--- ex025.py
+++ ex025.py
@@ -1,1 +1,5 @@
 #Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
+
+nome = str(input('Digite seu nome: ')).strip()
+print('Alnalizando seu nome...')
+print('Seu nome tem Silva? {}'.format('silva' in nome.lower()))
--- ex026.py
+++ ex026.py
@@ -1,1 +1,7 @@
 #Faça um programa que leia uma frase pelo teclado e mostre:                                   -> Quantas vezes aprarece a letra "A".                                                        -> Em que posição ela aparece a primeira vez.                                                                                           -> Em que posição ela aparece a última vez.
+
+frase = str(input('Digite um frase: ')).upper().strip()
+print('Analizando frase..')
+print('A letra A aparece {} vezes na frase.'.format(frase.count('A')))
+print('A primeira letra A parece na posição {}.'.format(frase.find('A')+1))
+print('A última letra A apareceu na posição {}.'.format(frase.rfind('A')+1))
