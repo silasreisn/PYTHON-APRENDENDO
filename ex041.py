a1e44b1feec46702d5b68b5215d48a32bf50c027
Perkles <147007042+Syllas3@users.noreply.github.com>
2026-01-29 19:10:28 -0300

    Novo exercicio

--- ex041.py
+++ ex041.py
@@ -3,3 +3,19 @@
 #Até 9 anos: MIRIM        Até 25 anos: SÊNIOR   
 #Até 14 anos: INFANTIL    Acima: MASTER
 #Até 19 anos: JÚNIOR 
+
+from datetime import date
+ano = int(input('Digite o ano de nascimento: '))
+print('\033[31mANALIZANDO...\033[m')
+idade = date.today().year - ano
+if idade <= 9:
+    print('Com a idade de {} anos, sua categoria é MIRIN.'.format(idade))
+elif idade <= 14:
+    print('Com a idade de {} anos, sua categoria é INFANTIL.'.format(idade))
+elif idade <= 19:
+    print('Com a idade de {} anos, sua categoria é JÚNIOR.'.format(idade))
+elif idade <= 25:
+    print('Com a idade de {} anos, sua categoria é SÊNIOR.'.format(idade))
+elif idade > 25:
+    print('Com a idade de {} anos, sua categoria é MASTER.'.format(idade))
+print(' ')
