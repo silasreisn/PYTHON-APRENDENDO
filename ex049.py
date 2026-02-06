7b8ee9a3cb9ae8780c03e0a6695b4070028b5e2a
Perkles <147007042+Syllas3@users.noreply.github.com>
2026-02-06 19:09:15 -0300

    Novo exercicio

--- ex049.py
+++ ex049.py
@@ -1,1 +1,6 @@
 #Refaça o DESAFIO 009, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
+
+num = int(input('Digite um número para ver na tabuada: '))
+for c in range(1, 11):
+    print('{} x {:2} = {}'.format(num, c, num*c))
+print(' ')
