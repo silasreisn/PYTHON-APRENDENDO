152617847579312a7e7d7e016d2e00179f1f4ccd
Perkles <147007042+Syllas3@users.noreply.github.com>
2026-02-07 08:57:31 -0300

    Novo exercicio

--- ex050.py
+++ ex050.py
@@ -1,1 +1,11 @@
 #Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que foram pares. Se o valor digitado for ímpar, desconsidere-o.
+
+soma = 0
+cont = 0
+for c in range(1, 7):
+    num = int(input(('Digite o {}° valor: '.format(c))))
+    if num % 2 == 0:
+        soma += num
+        cont += 1
+print('Você informou {} números PARES e a soma é {}'.format(cont, soma))
+print(' ')
