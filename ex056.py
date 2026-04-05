b096e9654e30d93f5e70176039361bd79cf2b965
Perkles <147007042+Syllas3@users.noreply.github.com>
2026-04-05 09:45:46 -0300

    Novo Exercicio

--- ex056.py
+++ ex056.py
@@ -2,3 +2,30 @@
 #A média de idade do grupo
 #Qual é o nome do homem mais velho.
 #Quantas mulheres têm menos de 20 anos.
+
+somaidade = 0
+mediaidade = 0
+maioridade = 0
+nomevelho = ''
+mulheresmenor20 = 0
+for p in range(1, 5):
+    print('----- {}ª PESSOA -----'.format(p))
+    nome = str(input('Nome: ')).strip()
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: ')).strip()
+    print()
+    somaidade += idade
+    if p == 1 and sexo in 'Mm':
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Mm' and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Ff' and idade < 20:
+        mulheresmenor20 += 1
+
+mediaidade = somaidade / 4
+print('A média de idade do grupo é de {} anos.'.format(mediaidade))
+print('O homem mais velho tem {} anos e se chama {}.'.format(maioridadehomem, nomevelho))
+print('Ao todo são {} mulheres com menos de 20 anos.'.format(mulheresmenor20))
+print()
