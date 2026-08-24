71b233748f01c0f76d73d9f63099b1e4ac4c9f22
Silas Reis <147007042+silasreisn@users.noreply.github.com>
2026-08-24 18:35:45 -0300

    Novas atividades baseado na aula 14

--- aula python/aula14a.py
+++ aula python/aula14a.py
@@ -12,7 +12,24 @@ print('Fim')'''
     n = int(input('Digite um valor: '))
 print('Fim')'''
 
-n = 1 
+'''n = 1 
 while n != 0:
     n = int(input('Digite um valor: '))
-print('Fim')
+print('Fim')'''
+
+'''r = 'S'
+while r == 'S':
+    n = int(input('Digite um valor: '))
+    r = str(input('Quero continuar? [S/N] ')).upper()
+print('Fim')'''
+
+n = 1
+par = impar = 0
+while n != 0:
+    n = int(input('Digite um valor: '))
+    if n != 0:
+        if n % 2 == 0:
+            par += 1
+        else:
+            impar += 1
+print('Você digitou {} números pares e {} números ímpares'.format(par, impar))
